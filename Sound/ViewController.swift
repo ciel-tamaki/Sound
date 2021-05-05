9eff95c4b1c7b5a337c0f23243cba3952a0741c4
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-05 18:25:16 +0900

    サウンドアプリ２の完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,25 +6,40 @@
 //  Copyright © 2021 安念　玉希. All rights reserved.
 //
 
+
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var drumButton: UIButton!
+    
     let drumSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name:"drumSound")!.data)
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func tapDrumButton(){
+
+    @IBAction func touchDownDrumButton(){
         
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+
+
         drumSoundPlayer.currentTime =  0
+        
         drumSoundPlayer.play()
-
-       }
-
+        
+    }
+    
+    @IBAction func touchUpDrumButton(){
+       drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+    
 
 }
+
+
 
